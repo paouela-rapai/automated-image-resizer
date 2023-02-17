9ad505f30e9d11844c481ad73d50e2a7241740ed
paouela-rapai <paouela.rapai@gmail.com>
2023-02-17 04:58:57 -0800

    refactor code to use a class ImageResizer

--- resizer.py
+++ resizer.py
@@ -4,25 +4,30 @@ from PIL import Image
 import sys
 
 
-def resize_image(path, size):
-    output_dir = os.path.join(path, 'resized')
-    os.makedirs(output_dir, exist_ok=True)
-    image_files = []
-    for extension in ["*.jpg", "*.jpeg", "*.jfif", ".pjpeg", "*.pjp", "*.png", "*.gif"]:
-        image_files.extend(glob.glob(os.path.join(path, "**", extension), recursive=True))
-    print(len(image_files))
-    for image in image_files:
-        with Image.open(image) as img:
-            img.thumbnail(size)
-            output_image = os.path.join(output_dir, os.path.basename(image))
-            img.save(output_image)
-            print(f"{image} resized to {size} and saved as {output_image}")
+class ImageResizer:
+    def __init__(self, path, horizontal_size, vertical_size):
+        self.path = path
+        self.size = (int(horizontal_size), int(vertical_size))
+
+    def resize_images(self):
+        output_dir = os.path.join(self.path, 'resized')
+        os.makedirs(output_dir, exist_ok=True)
+        image_files = []
+        for extension in ["*.jpg", "*.jpeg", "*.jfif", ".pjpeg", "*.pjp", "*.png", "*.gif"]:
+            image_files.extend(glob.glob(os.path.join(self.path, "**", extension), recursive=True))
+        for image in image_files:
+            with Image.open(image) as img:
+                img.thumbnail(self.size)
+                output_image = os.path.join(output_dir, os.path.basename(image))
+                img.save(output_image)
+                print(f"{image} resized to {self.size} and saved as {output_image}")
+
 
 if __name__ == '__main__':
     PATH = sys.argv[1]
     HORIZONTAL_SIZE = sys.argv[2]
     VERTICAL_SIZE = sys.argv[3]
-    size = (int(HORIZONTAL_SIZE), int(VERTICAL_SIZE))
     
-    resize_image(PATH, size)
+    image_resizer = ImageResizer(PATH, HORIZONTAL_SIZE, VERTICAL_SIZE)
+    image_resizer.resize_images()
 

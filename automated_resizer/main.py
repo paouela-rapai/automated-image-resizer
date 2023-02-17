ccb562d8e56dea425478ebfe01842f06d1b9fbee
paouela-rapai <paouela.rapai@gmail.com>
2023-02-17 08:10:37 -0800

    refactor code to run concurrently with ThreadPoolExecutor

--- automated_resizer/main.py
+++ automated_resizer/main.py
@@ -1,14 +1,12 @@
 import sys
 from utils.image_resizer import ImageResizer
-import concurrent.futures
+
 
 if __name__ == '__main__':
+
     PATH = sys.argv[1]
     HORIZONTAL_SIZE = sys.argv[2]
     VERTICAL_SIZE = sys.argv[3]
     
     image_resizer = ImageResizer(PATH, HORIZONTAL_SIZE, VERTICAL_SIZE)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        executor.map(image_resizer.resize_images(), range(3))
-
-    
+    image_resizer.resize_images()
--- automated_resizer/utils/image_resizer.py
+++ automated_resizer/utils/image_resizer.py
@@ -1,6 +1,7 @@
 import os
 import glob
 from PIL import Image
+import concurrent.futures
 
 
 class ImageResizer:
@@ -8,15 +9,24 @@ class ImageResizer:
         self.path = path
         self.size = (int(horizontal_size), int(vertical_size))
 
-    def resize_images(self):
-        output_dir = os.path.join(self.path, 'resized')
-        os.makedirs(output_dir, exist_ok=True)
+    def traverse_directory(self):
         image_files = []
         for extension in ["*.jpg", "*.jpeg", "*.jfif", ".pjpeg", "*.pjp", "*.png", "*.gif"]:
             image_files.extend(glob.glob(os.path.join(self.path, "**", extension), recursive=True))
-        for image in image_files:
-            with Image.open(image) as img:
-                img.thumbnail(self.size)
-                output_image = os.path.join(output_dir, os.path.basename(image))
-                img.save(output_image)
-                print(f"{image} resized to {self.size} and saved as {output_image}")
+        return image_files
+        
+    def resize_image(self, image):
+        with Image.open(image) as img:
+            img.thumbnail(self.size)
+            self.output_dir = os.path.join(self.path, 'resized')
+            os.makedirs(self.output_dir, exist_ok=True)
+            output_image = os.path.join(self.output_dir, os.path.basename(image))
+            img.save(output_image)
+            print(f"{image} resized to {self.size} and saved as {output_image}")
+
+    def resize_images(self):
+        image_files = self.traverse_directory()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.resize_image, image) for image in image_files]
+            for future in concurrent.futures.as_completed(futures):
+                future.result()
